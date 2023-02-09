21854d219ff57750f6d343d11775dec411484dcd
syedrafayjavaid <syedrafayjavaid16@gmail.com>
2023-02-09 16:47:45 +0500

    updated server.js file

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,10 @@
-import {ConfigKey} from "config/config-keys";
+import { ConfigKey } from "config/config-keys";
 
-const serverConfig = ({env}) => ({
+const serverConfig = ({ env }) => ({
     host: env(ConfigKey.host, 'localhost'),
     port: env.int(ConfigKey.port, 3001),
     proxy: true,
-    url: env(ConfigKey.baseUrl, `http://localhost:3001`),
+    url: env(ConfigKey.baseUrl, `http://23.20.82.14:3001`),
     admin: {
         autoOpen: false,
         auth: {
