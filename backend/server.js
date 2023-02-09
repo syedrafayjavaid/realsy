9b5bb1c48361b2142247fa9c1828b301d6567596
syedrafayjavaid <syedrafayjavaid16@gmail.com>
2023-02-09 15:50:02 +0500

    updated server file

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,10 @@ const server = strapi({
     autoReload: process.env.NODE_ENV === 'development',
 });
 
+server.app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 server.start().then(() => console.log('API started'));
