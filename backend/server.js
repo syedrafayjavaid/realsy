110b763e8cfa00ce755a937a0f0379488fa7f506
syedrafayjavaid <syedrafayjavaid16@gmail.com>
2023-02-09 15:56:47 +0500

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,4 @@ const server = strapi({
     autoReload: process.env.NODE_ENV === 'development',
 });
 
-server.app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 server.start().then(() => console.log('API started'));
