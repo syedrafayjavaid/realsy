841c3f278ee4f5a5cbb28ed8ba340eeb9f0f4f10
syedrafayjavaid <syedrafayjavaid16@gmail.com>
2023-02-13 15:15:18 +0500

    updaetd server.js file

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,8 +3,8 @@ import { ConfigKey } from "config/config-keys";
 const serverConfig = ({ env }) => ({
     host: env(ConfigKey.host, 'localhost'),
     port: env.int(ConfigKey.port, 3001),
-    proxy: false,
-    url: env(ConfigKey.baseUrl, `http://localhost:3001`),
+    proxy: true,
+    url: 'https://realsy.homes',
     admin: {
         autoOpen: false,
         auth: {
