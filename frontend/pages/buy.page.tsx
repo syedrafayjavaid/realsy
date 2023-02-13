5de9168707b506cb4c59ed860bdcb1c8ca099108
syedrafayjavaid <syedrafayjavaid16@gmail.com>
2023-02-13 19:08:42 +0500

    updated

--- frontend/pages/buy.page.tsx
+++ frontend/pages/buy.page.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ListingsMap from 'components/listings/map';
-import {useMainLayout} from "layout/main-layout";
+import { useMainLayout } from "layout/main-layout";
 
 /**
  * The "buy" page
